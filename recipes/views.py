2fab59dac6582b8b22ebffc32554260277b19124
KaikeRochaDev <kaikerochadev@gmail.com>
2023-02-16 15:28:35 -0300

    removed contact and about

--- recipes/views.py
+++ recipes/views.py
@@ -1,13 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
     return render(request, 'home.html', {'name': 'KaikeRochaDev'})
-
-def sobre(request):
-    return HttpResponse('Sobre')
-
-
-def contato(request):
-    return HttpResponse('Contato')
