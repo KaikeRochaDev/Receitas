49e30acef062f70859dcc3f2f959e69f08e63ec3
KaikeRochaDev <kaikerochadev@gmail.com>
2023-02-16 15:53:54 -0300

    change home location & splitting the head partials

--- recipes/views.py
+++ recipes/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return render(request, 'home.html', {'name': 'KaikeRochaDev'})
+    return render(request, 'recipes/pages/home.html', {'name': 'KaikeRochaDev'})
