ba449749281f277b10c1ab5ef52b3d3db83b8132
KaikeRochaDev <kaikerochadev@gmail.com>
2023-02-17 00:01:15 -0300

    creating page recipe-view

--- recipes/views.py
+++ recipes/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+
 def home(request):
     return render(request, 'recipes/pages/home.html', {'name': 'KaikeRochaDev'})
+
+def recipe(request, id):
+    return render(request, 'recipes/pages/recipe-view.html')
