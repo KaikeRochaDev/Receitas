bf29a549c2ba07742bae82a5e315b306962c317a
KaikeRochaDev <kaikerochadev@gmail.com>
2023-02-18 14:44:07 -0300

    adding functionality to category urls

--- recipes/views.py
+++ recipes/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
 from utils.recipes.factory import make_recipe
-
+from .models import Recipe
 
 def home(request):
-    return render(request, 'recipes/pages/home.html', {'recipes': [make_recipe() for _ in range(10)]})
+    recipes = Recipe.objects.all().order_by('-id')
+    return render(request, 'recipes/pages/home.html', {'recipes': recipes})
+
+def category(request, category_id):
+    recipes = Recipe.objects.filter(category__id=category_id).order_by('-id')
+    return render(request, 'recipes/pages/home.html', {'recipes': recipes})
 
 def recipe(request, id):
     return render(request, 'recipes/pages/recipe-view.html', {'recipe': make_recipe(), 'is_detail_page': True})
