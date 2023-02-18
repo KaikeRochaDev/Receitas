21cc44c591c341297a649e7dbf251740d9b7ad2a
KaikeRochaDev <kaikerochadev@gmail.com>
2023-02-17 23:44:17 -0300

    Registering models in the admin

--- recipes/models.py
+++ recipes/models.py
@@ -3,6 +3,9 @@ from django.contrib.auth.models import User
 
 class Category(models.Model):
     name = models.CharField(max_length=65)
+    
+    def __str__(self):
+        return self.name
 
 
 class Recipe(models.Model):
@@ -23,4 +26,4 @@ class Recipe(models.Model):
     category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
     
     author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
-    
+
