bb4662ef9224b2ef80d951860801d8960eaebac5
KaikeRochaDev <kaikerochadev@gmail.com>
2023-02-18 14:50:28 -0300

    hide recipe that no publish & create page category

--- recipes/views.py
+++ recipes/views.py
@@ -3,12 +3,12 @@ from utils.recipes.factory import make_recipe
 from .models import Recipe
 
 def home(request):
-    recipes = Recipe.objects.all().order_by('-id')
+    recipes = Recipe.objects.filter(is_published=True).order_by('-id')
     return render(request, 'recipes/pages/home.html', {'recipes': recipes})
 
 def category(request, category_id):
-    recipes = Recipe.objects.filter(category__id=category_id).order_by('-id')
-    return render(request, 'recipes/pages/home.html', {'recipes': recipes})
+    recipes = Recipe.objects.filter(category__id=category_id, is_published=True).order_by('-id')
+    return render(request, 'recipes/pages/category.html', {'recipes': recipes})
 
 def recipe(request, id):
     return render(request, 'recipes/pages/recipe-view.html', {'recipe': make_recipe(), 'is_detail_page': True})
