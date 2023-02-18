08b5335cb0df03dadde7d62162c767f87a7494f4
KaikeRochaDev <kaikerochadev@gmail.com>
2023-02-18 18:42:44 -0300

    showing error 404

--- recipes/views.py
+++ recipes/views.py
@@ -8,7 +8,8 @@ def home(request):
 
 def category(request, category_id):
     recipes = Recipe.objects.filter(category__id=category_id, is_published=True).order_by('-id')
-    return render(request, 'recipes/pages/category.html', {'recipes': recipes})
+    
+    return render(request, 'recipes/pages/category.html', {'recipes': recipes, 'title': f'{recipes.first().category.name} - Categoria | '})
 
 def recipe(request, id):
     return render(request, 'recipes/pages/recipe-view.html', {'recipe': make_recipe(), 'is_detail_page': True})
