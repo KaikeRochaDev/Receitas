fe897554d9c4c70b0f77e942d5abcc1852836d79
KaikeRochaDev <kaikerochadev@gmail.com>
2023-02-19 18:10:13 -0300

    extracting methods

--- recipes/tests/test_recipe_views.py
+++ recipes/tests/test_recipe_views.py
@@ -1,13 +1,14 @@
-from django.urls import reverse, resolve
+from django.urls import resolve, reverse
 from recipes import views
-from recipes.models import Recipe
+
+from .test_recipe_base import RecipeTestBase
 from .test_recipe_base import RecipeTestBase
 
+
 class RecipeViewsTest(RecipeTestBase):
-    # TearDown sempre é iniciado no fim de uma função
     def tearDown(self) -> None:
         return super().tearDown()
-    
+
     def test_recipe_home_view_function_is_correct(self):
         view = resolve(reverse('recipes:home'))
         self.assertIs(view.func, views.home)
@@ -21,35 +22,43 @@ class RecipeViewsTest(RecipeTestBase):
         self.assertTemplateUsed(response, 'recipes/pages/home.html')
         
     def test_recipe_home_template_shows_no_recipes_found_if_no_recipes(self):
-        Recipe.objects.get(id=1).delete()
         response = self.client.get(reverse('recipes:home'))
         self.assertIn(
-            'Nenhuma receita foi encontrada aqui 😞', 
-            response.content.decode('utf-8'))
-        
+            'Nenhuma receita foi encontrada aqui 😞',
+            response.content.decode('utf-8')
+        )
+
     def test_recipe_home_template_loads_recipes(self):
+        # Need a recipe for this test
+        self.make_recipe()
+
         response = self.client.get(reverse('recipes:home'))
         content = response.content.decode('utf-8')
         response_context_recipes = response.context['recipes']
+
+        # Check if one recipe exists
         self.assertIn('Recipe Title', content)
         self.assertIn('10 Minutos', content)
         self.assertIn('5 Porções', content)
         self.assertEqual(len(response_context_recipes), 1)
-        ...
-    
+
     def test_recipe_category_view_function_is_correct(self):
-        view = resolve(reverse('recipes:category', kwargs={'category_id': 1}))
+        view = resolve(
+            reverse('recipes:category', kwargs={'category_id': 1000})
+        )
         self.assertIs(view.func, views.category)
-        
     def test_recipe_category_view_returns_404_if_no_recipes_found(self):
-        response = self.client.get(reverse('recipes:category', kwargs={'category_id': 1000}))
+        response = self.client.get(
+            reverse('recipes:category', kwargs={'category_id': 1000})
+        )
         self.assertEqual(response.status_code, 404)
-        
     def test_recipe_detail_view_function_is_correct(self):
-        view = resolve(reverse('recipes:recipe', kwargs={'id': 1}))
+        view = resolve(
+            reverse('recipes:recipe', kwargs={'id': 1})
+        )
         self.assertIs(view.func, views.recipe)
-        
     def test_recipe_detail_view_returns_404_if_no_recipes_found(self):
-        response = self.client.get(reverse('recipes:recipe', kwargs={'id': 1000}))
+        response = self.client.get(
+            reverse('recipes:recipe', kwargs={'id': 1000})
+        )
         self.assertEqual(response.status_code, 404)
-        
