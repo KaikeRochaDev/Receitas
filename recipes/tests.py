59922dfc7be5fcf865b1e96e27808969f2f0b9e4
KaikeRochaDev <kaikerochadev@gmail.com>
2023-02-19 10:40:51 -0300

    test_recipe_home_url_is_correct

--- recipes/tests.py
+++ recipes/tests.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
+from django.urls import reverse
 
-class RecipeURLsTest(TestCase):
-    def test_the_pytest_is_ok(self):
-        print('Olá mundo')
-        assert 1 == 1, 'Um é igual a um'
+class RecipeURLsTest(TestCase):     
+    def test_recipe_home_url_is_correct(self):
+        home_url = reverse('recipes:home')
+        self.assertEqual(home_url, '/')
