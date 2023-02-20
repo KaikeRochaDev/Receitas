b9c36dd21a44de61f6bdc2ce54ce22cb8153ae66
KaikeRochaDev <kaikerochadev@gmail.com>
2023-02-19 23:14:23 -0300

    creating view search

--- recipes/tests/test_recipe_views.py
+++ recipes/tests/test_recipe_views.py
@@ -113,3 +113,7 @@ class RecipeViewsTest(RecipeTestBase):
         response = self.client.get(reverse('recipes:recipe', kwargs={'id': recipe.category.id}))
         
         self.assertEqual(response.status_code, 404)
+        
+    def test_recipe_search_users_correct_view_function(self):
+        resolved = resolve(reverse('recipes:search'))
+        self.assertIs(resolved.func, views.search)
