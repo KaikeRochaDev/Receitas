6ef290d952b2a099c076f5f0186b251c2c496247
KaikeRochaDev <kaikerochadev@gmail.com>
2023-02-21 00:37:07 -0300

    configuring the form

--- authors/forms.py
+++ authors/forms.py
@@ -5,3 +5,33 @@ class RegisterForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ['first_name', 'last_name', 'username', 'email', 'password']
+        
+        labels = {
+            'username': 'Nome de usuário',
+            'first_name': 'Nome',
+            'last_name': 'Sobrenome',
+            'email': 'E-mail',
+            'password': 'Senha',
+        }
+        help_texts = {
+            'email': 'Digite um e-mail válido'
+        }
+        
+        error_messages = {
+            'username': {
+                'required': 'Este campo é obrigatório',
+            }
+        }
+        
+        widgets = {
+            'first_name': forms.TextInput(),
+            
+            'last_name': forms.TextInput(),
+            
+            'username': forms.TextInput(),
+            
+            'email': forms.EmailInput(),
+            
+            'password': forms.PasswordInput()
+        }
+        
