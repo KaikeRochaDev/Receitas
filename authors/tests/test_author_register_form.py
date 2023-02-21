291dfc95d3afd593686d0f1e8a298dd20d941ba4
KaikeRochaDev <kaikerochadev@gmail.com>
2023-02-21 13:24:27 -0300

    testing the help_text

--- authors/tests/test_author_register_form.py
+++ authors/tests/test_author_register_form.py
@@ -11,7 +11,15 @@ class AuthorRegisterFormUnitTest(TestCase):
         ('password', 'Digite sua senha...'),
         ('confirm_password', 'Repita sua senha...'),
     ])
-    def test_first_name_placeholder_is_correct(self, field, placeholder):
+    def test_fields_placeholder(self, field, placeholder):
         form = RegisterForm()
         current_placeholder = form[field].field.widget.attrs['placeholder']
         self.assertEqual(current_placeholder, placeholder)
+        
+    @parameterized.expand([
+        ('email', 'Digite um e-mail válido')
+    ])
+    def test_fields_help_text(self, field, needed):
+        form = RegisterForm()
+        current = form[field].field.help_text
+        self.assertEqual(current, needed)
