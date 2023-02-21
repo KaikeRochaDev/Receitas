d48759228a8c3590581418f71faa95d4e84d3ece
KaikeRochaDev <kaikerochadev@gmail.com>
2023-02-21 01:12:04 -0300

    reusing a function

--- authors/forms.py
+++ authors/forms.py
@@ -6,7 +6,7 @@ def add_attr(field, attr_name, attr_new_val):
     field.widget.attrs[attr_name] = f'{existing_attr} {attr_new_val}'.strip()
     
 def add_placeholder(field, placeholder_val):
-    field.widget.attrs['placeholder'] = placeholder_val
+    add_attr(field, 'placeholder', placeholder_val)
 
 class RegisterForm(forms.ModelForm):
     confirm_password = forms.CharField(
