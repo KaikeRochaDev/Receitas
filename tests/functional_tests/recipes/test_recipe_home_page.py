88b9c5f889b0dbd14f43187ba2c00ebb9e7495c6
KaikeRochaDev <kaikerochadev@gmail.com>
2023-02-22 18:58:22 -0300

    creating a function that creates multiple recipes

--- tests/functional_tests/recipes/test_recipe_home_page.py
+++ tests/functional_tests/recipes/test_recipe_home_page.py
@@ -4,16 +4,17 @@ from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 import time
 import pytest
 from recipes.tests.test_recipe_base import RecipeMixin
+from unittest.mock import patch
 
 @pytest.mark.functional_test
 class RecipeHomePageFunctionalTest(StaticLiveServerTestCase, RecipeMixin):
     def sleep(self, seconds=5):
         time.sleep(seconds)
 
+    @patch('recipes.views.PER_PAGE', new=2)
     def test_recipe_home_page_without_recipes_not_found_message(self):
         browser = make_chrome_browser()
         browser.get(self.live_server_url)
         body = browser.find_element(By.TAG_NAME, 'body')
-        
         self.assertIn('Nenhuma receita foi encontrada aqui 😞', body.text)
         browser.quit()
