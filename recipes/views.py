8523fd230007c03087afdfba28cf0af464a61781
KaikeRochaDev <kaikerochadev@gmail.com>
2023-02-25 23:19:18 -0300

    removing unusable imports

--- recipes/views.py
+++ recipes/views.py
@@ -1,6 +1,5 @@
 from django.db.models import Q
 from django.http.response import Http404
-from django.shortcuts import get_list_or_404, get_object_or_404, render
 from utils.pagination import make_pagination
 import os
 from recipes.models import Recipe
@@ -88,27 +87,6 @@ class RecipeListViewSearch(RecipeListViewBase):
         })
 
         return ctx
-    
-def home(request):
-    recipes = Recipe.objects.filter(
-        is_published=True,
-    ).order_by('-id')
-    
-    page_obj, pagination_range = make_pagination(request, recipes, PER_PAGE)
-
-    return render(request, 'recipes/pages/home.html', context={
-        'recipes': page_obj,
-        'pagination_range': pagination_range
-    })
-
-def recipe(request, id):
-    recipe = get_object_or_404(Recipe, id=id, is_published=True,)
-
-    return render(request, 'recipes/pages/recipe-view.html', context={
-        'recipe': recipe,
-        'is_detail_page': True,
-    })
-    
 class RecipeDetail(DetailView):
     model = Recipe
     context_object_name = 'recipe'
