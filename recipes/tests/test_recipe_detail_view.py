286b59a140b0f50d88796687620ac9cf268b1eff
KaikeRochaDev <kaikerochadev@gmail.com>
2023-02-25 23:26:29 -0300

    fixing the detail view tests

--- recipes/tests/test_recipe_detail_view.py
+++ recipes/tests/test_recipe_detail_view.py
@@ -6,13 +6,13 @@ from .test_recipe_base import RecipeTestBase
 class RecipeDetailViewTest(RecipeTestBase):
     def test_recipe_detail_view_function_is_correct(self):
         view = resolve(
-            reverse('recipes:recipe', kwargs={'id': 1})
+            reverse('recipes:recipe', kwargs={'pk': 1})
         )
-        self.assertIs(view.func, views.recipe)
+        self.assertIs(view.func.view_class, views.RecipeDetail)
         
     def test_recipe_detail_view_returns_404_if_no_recipes_found(self):
         response = self.client.get(
-            reverse('recipes:recipe', kwargs={'id': 1000})
+            reverse('recipes:recipe', kwargs={'pk': 1000})
         )
         self.assertEqual(response.status_code, 404)
         
@@ -21,7 +21,7 @@ class RecipeDetailViewTest(RecipeTestBase):
         # Need a recipe for this test
         self.make_recipe(title=needed_title)
 
-        response = self.client.get(reverse('recipes:recipe', kwargs={'id': 1}))
+        response = self.client.get(reverse('recipes:recipe', kwargs={'pk': 1}))
         content = response.content.decode('utf-8')
 
         # Check if one recipe exists
@@ -30,7 +30,7 @@ class RecipeDetailViewTest(RecipeTestBase):
     def test_recipe_detail_template_dont_load_recipe_not_published(self):
         recipe = self.make_recipe(is_published=False)
 
-        response = self.client.get(reverse('recipes:recipe', kwargs={'id': recipe.category.id}))
+        response = self.client.get(reverse('recipes:recipe', kwargs={'pk': recipe.category.id}))
         
         self.assertEqual(response.status_code, 404)
         
